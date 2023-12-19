556f06353dd577b091ea73b7035e9a535c10ad84
Nguyen Minh Nghia <ngh95411@gmail.com>
2023-12-19 09:42:24 +0700

    update

---
+++ Assignment1 PLSQL/WindowsFormsApp/menuForm.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace WindowsFormsApp
+{
+    partial class menuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.Group1 = new System.Windows.Forms.GroupBox();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.Group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(389, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "MENU";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.DimGray;
+            this.button12.ForeColor = System.Drawing.Color.White;
+            this.button12.Location = new System.Drawing.Point(31, 731);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(787, 58);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "CLOSE";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(169, 415);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(234, 40);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "GET_ALLPROD";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(463, 415);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(234, 40);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "GET_ALLCUST";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(584, 586);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(234, 40);
+            this.button20.TabIndex = 28;
+            this.button20.Text = "DELETE_ALL_SALES";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(308, 586);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(234, 40);
+            this.button19.TabIndex = 27;
+            this.button19.Text = "DELETE_SALE";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(32, 586);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(234, 40);
+            this.button18.TabIndex = 26;
+            this.button18.Text = "COUNT_PROD_SALES";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(584, 504);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(234, 40);
+            this.button17.TabIndex = 25;
+            this.button17.Text = "GET_ALLSALES";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(308, 504);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(234, 40);
+            this.button16.TabIndex = 24;
+            this.button16.Text = "ADD_COMPLEX_SALE";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(31, 504);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(234, 40);
+            this.button15.TabIndex = 23;
+            this.button15.Text = "ADD_LOCATION";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD_CUST_TO_DB ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DELETE_ALL_PROD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DELETE_ALL_CUST";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(283, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "GET_CUST_STRING";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(560, 42);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(234, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "ADD_PROD_TO_DB";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(560, 110);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(234, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "UPD_CUST_SALESYTD";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(284, 182);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(234, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "UPD_PROD_SALES";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(7, 182);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(234, 40);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "GET_PROD_STRING";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(559, 182);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(234, 40);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "UPD_CUST_STATUS";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(144, 252);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(234, 40);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "ADD_SIMPLE_SALE";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(438, 252);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(234, 40);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "SUM_CUST_SALE";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Group1
+            // 
+            this.Group1.Controls.Add(this.button11);
+            this.Group1.Controls.Add(this.button10);
+            this.Group1.Controls.Add(this.button9);
+            this.Group1.Controls.Add(this.button8);
+            this.Group1.Controls.Add(this.button7);
+            this.Group1.Controls.Add(this.button6);
+            this.Group1.Controls.Add(this.button5);
+            this.Group1.Controls.Add(this.button4);
+            this.Group1.Controls.Add(this.button3);
+            this.Group1.Controls.Add(this.button2);
+            this.Group1.Controls.Add(this.button1);
+            this.Group1.Location = new System.Drawing.Point(28, 53);
+            this.Group1.Name = "Group1";
+            this.Group1.Size = new System.Drawing.Size(814, 324);
+            this.Group1.TabIndex = 22;
+            this.Group1.TabStop = false;
+            this.Group1.Enter += new System.EventHandler(this.Group1_Enter);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(169, 666);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(234, 40);
+            this.button21.TabIndex = 29;
+            this.button21.Text = "DELETE_CUSTOMER";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(448, 666);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(234, 40);
+            this.button22.TabIndex = 30;
+            this.button22.Text = "DELETE_PROD";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // menuForm
+            // 
+            this.ClientSize = new System.Drawing.Size(854, 809);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.Group1);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label2);
+            this.Name = "menuForm";
+            this.Group1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.GroupBox Group1;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+    }
+}
